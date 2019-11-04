44940751d5c35e5ce1b9932f44e53b994991c556
mag987 <mag987@list.ru>
2019-11-04 22:53:10 +0700

    added test methods for Factorial events

--- MSTestFactorial/UnitTest1.cs
+++ MSTestFactorial/UnitTest1.cs
@@ -24,6 +24,25 @@ namespace MSTestFactorial
                 f.GetType().GetEvent("Initialized").EventHandlerType.Name
                 );
         }
+        [TestMethod]
+        public void InitializedEventArgumentType()
+        {
+            Factorial.Factorial f = new Factorial.Factorial(3);
+            f.Initialized += (fact) => {
+                Assert.AreEqual(fact.GetType(), typeof(Factorial.Factorial));
+            };
+            f.Start();
+        }
+        [TestMethod]
+        public void InitializedEventArgumentValues()
+        {
+            Factorial.Factorial f = new Factorial.Factorial(3);
+            f.Initialized += (fact) => {
+                Assert.AreEqual(fact.Number,3);
+                Assert.AreEqual(fact.Result, null);
+            };
+            f.Start();
+        }
     }
     public class EventRaisedExeption : Exception
     {
